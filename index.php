05a292fbccc93ddb400ad0bfe48c7ae869f81a83
jere8982 <jeremy.arntz@rackspace.com>
2016-03-21 16:40:25 -0500

    Moved config data to config file and cleaned things up a bit

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
-require 'vendor/autoload.php';
+// load config variables
+$configArray = parse_ini_file("config.ini");
 
-$key = '';
-$secret = '';
-$token = '';
-$userName = '';
+require 'vendor/autoload.php';
 
 // TODO: Figure this out
 //['headers' => [ 'Authorization' => 'Discogs', 'token' => $token]]
 
 $client = new GuzzleHttp\Client();
 
-// https://api.discogs.com/database/users/{username}/collection/
-
 // https://api.discogs.com/database/search
 // 'q' => 'Nirvana',
 
 $res = $client->request('GET',
-                        'https://api.discogs.com/users/'.$userName.'/collection/folders/0/releases',
-                        ['query' => ['token' => $token]]
+                        'https://api.discogs.com/users/'.$configArray['userName'].'/collection/folders/0/releases',
+                        ['query' => ['token' => $configArray['token']]]
                         );
 
-echo '<pre>';
-var_dump(json_decode($res->getBody(), true));
-echo '</pre>';
+$data = json_decode($res->getBody(), true);
+
+foreach ($data['releases'] AS $release) {
+  echo $release['basic_information']['title'].'<br />';
+}
+
+// echo '<pre>';
+// var_dump($data['releases']);
+// echo '</pre>';
 ?>
